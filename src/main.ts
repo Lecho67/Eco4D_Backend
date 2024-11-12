44b251cc80d6dd5a8d291fbc594a73b7b09e48c3
Juanestebane926 <juan.cuello@uao.edu.co>
2024-11-12 12:30:33 -0500

    permitiendo que le enviend credenciales desde el front-end a la api

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,10 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
-  app.enableCors()
+  app.enableCors({
+    origin: true,
+    credentials: true
+  })
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
