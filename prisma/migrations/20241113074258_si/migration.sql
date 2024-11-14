5bbaa2a18b980f59815e3f94b3e45b98cc991a7c
Simon Colonia Amador <simoncolonia67@gmail.com>
2024-11-13 19:12:07 -0500

    Delete migration.sql

--- prisma/migrations/20241113074258_si/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Changed the type of `edadGestacional` on the `Diagnostico` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
-
-*/
--- AlterTable
-ALTER TABLE "Diagnostico" DROP COLUMN "edadGestacional",
-ADD COLUMN     "edadGestacional" INTEGER NOT NULL;
